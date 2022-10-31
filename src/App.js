3fe6af658783e34ebf8120d3c4bf971df863d0ec
Hassan Khan <hassankhan427726@outlook.com>
2022-10-31 22:02:01 +0500

    Chakra UI installed & onchange set

--- src/App.js
+++ src/App.js
@@ -3,42 +3,64 @@ import React, { useEffect, useState } from "react";
 // import { SECRET_KEY } from "./constants";
 import { Base64 } from "./base64";
 import FileBase from "react-file-base64";
+import {
+  ChakraProvider,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Button,
+  useDisclosure,
+  AlertDialogCloseButton,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Flex,
+  Input,
+} from "@chakra-ui/react";
 
 export default function App() {
+  // Name of image
   const [seletedFile, setSelectedFile] = useState(null);
+  // Base64 of image
   const [singleFile, setSingleFile] = useState([]);
 
   useEffect(() => {
-    console.log(singleFile);
+    // console.log(singleFile);
     if (singleFile.selectedFile !== undefined) {
       let base64Pure = singleFile.selectedFile.slice(21);
-      console.log(base64Pure);
+      // console.log(base64Pure);
     }
-    document
-      .querySelectorAll('input[type="file"]')[1]
-      .setAttribute("accept", "image/png, image/gif, image/jpeg");
+    // document.querySelector('input[type="file"]').onchange = function () {
+    //   console.log("Selected file: " + this.value);
+    // };
+    let file = document.querySelectorAll('input[type="file"]')[0];
+    file.setAttribute("accept", "image/png, image/gif, image/jpeg");
+    file.onchange = function () {
+      console.log(this.value);
+    };
   }, [singleFile]);
 
   const convertJPEGtoWEBP = async (event) => {
     event.preventDefault();
-    const formData = new FormData();
-    formData.append("seletedFile", seletedFile);
-    console.log(formData);
+    // const formData = new FormData();
+    // formData.append("seletedFile", seletedFile);
+    // console.log(formData);
     try {
       const url = `https://v2.convertapi.com/convert/jpeg/to/webp?Secret=${process.env.REACT_APP_API_KEY}&StoreFile=true`;
       const res = await axios.post(url, {
         headers: {
           "Content-Type": "application/json",
         },
-        // data: {},
         Parameters: [
           {
             Name: "File",
             FileValue: {
-              // Name: seletedFile.name,
-              // Data: "<Base64 encoded file content>",
               Name: "test.jpg",
-              Data: Base64,
+              Data: singleFile,
             },
           },
           {
@@ -59,26 +81,26 @@ export default function App() {
   };
   return (
     <>
-      <img src={`data:image/jpeg;base64,${Base64}`} alt="" />
-      <form method="POST" onSubmit={convertJPEGtoWEBP}>
-        <label htmlFor="">Upload some file</label>
-        <br />
-        <input type="file" name="" id="" onChange={handleFileSelect} />
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-
-      <h1>Form 2 BASE64</h1>
-      {/* <h2>SECRET KEY = {process.env.REACT_APP_API_KEY}</h2> */}
-      <form onSubmit={(e) => e.preventDefault()}>
-        <FileBase
-          type="file"
-          multiple={false}
-          onDone={({ base64 }) =>
-            setSingleFile({ ...singleFile, selectedFile: base64 })
-          }
-        />
-      </form>
+      <ChakraProvider>
+        {/* <img src={`data:image/jpeg;base64,${Base64}`} alt="" /> */}
+        <form method="POST" onSubmit={convertJPEGtoWEBP}>
+          <FormControl>
+            {/* <input type="file" name="" id="" onChange={handleFileSelect} /> */}
+            <Flex direction="column">
+              <FormLabel>Select A File</FormLabel>
+              <FileBase
+                type="file"
+                multiple={false}
+                onDone={({ base64 }) =>
+                  setSingleFile({ ...singleFile, selectedFile: base64 })
+                }
+                onChange={handleFileSelect}
+              />
+              <Button type="submit">Submit</Button>
+            </Flex>
+          </FormControl>
+        </form>
+      </ChakraProvider>
     </>
   );
 }
