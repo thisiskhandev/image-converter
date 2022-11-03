712fc02dcf4e0a23322d67ea37be462ad8e864a7
Hassan Khan <hassankhan427726@outlook.com>
2022-11-03 22:03:32 +0500

    Uploading working

--- src/App.js
+++ src/App.js
@@ -20,29 +20,65 @@ import {
   FormHelperText,
   Flex,
   Input,
+  useToast,
+  Wrap,
+  WrapItem,
+  Image,
+  Skeleton,
+  Container,
+  Heading,
+  Box,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
 } from "@chakra-ui/react";
-
+import "./App.css";
 export default function App() {
   // Name of image
-  const [seletedFile, setSelectedFile] = useState(null);
-  // Base64 of image
+  const [seletedFileName, setSelectedFileName] = useState(null);
+  // Getting image from input field
   const [singleFile, setSingleFile] = useState([]);
+  // Converting image into base64
+  const [singleFileBase64, setSinglFileBase64] = useState();
+  // Button Loading
+  const [btnLoading, setBtnLoading] = useState(false);
+  // Loading
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    // console.log(singleFile);
     if (singleFile.selectedFile !== undefined) {
-      let base64Pure = singleFile.selectedFile.slice(21);
-      // console.log(base64Pure);
+      let base64Pure = singleFile.selectedFile.slice(22);
+      // jpg
+      // let base64Pure = singleFile.selectedFile.slice(23);
+      setSinglFileBase64(base64Pure);
+      // console.log(singleFileBase64);
     }
-    // document.querySelector('input[type="file"]').onchange = function () {
-    //   console.log("Selected file: " + this.value);
-    // };
+
     let file = document.querySelectorAll('input[type="file"]')[0];
-    file.setAttribute("accept", "image/png, image/gif, image/jpeg");
-    file.onchange = function () {
-      console.log(this.value);
+    // file.setAttribute("accept", "image/png, image/gif, image/jpeg");
+    file.setAttribute("accept", "image/png");
+    file.setAttribute("id", "imageShot");
+    file.onchange = function (e) {
+      setSelectedFileName(e.target.value.slice(12));
     };
-  }, [singleFile]);
+    // let triggerImageShot = document.getElementById("triggerImageShot");
+    // triggerImageShot.addEventListener("click", () => {
+    //   // file.addEventListener("click", (e) => console.log(e));
+    // });
+  }, [singleFile, singleFileBase64]);
+
+  useEffect(() => {
+    if (seletedFileName === null || seletedFileName === undefined) {
+      return;
+    }
+    console.log(seletedFileName);
+  }, [seletedFileName]);
 
   const convertJPEGtoWEBP = async (event) => {
     event.preventDefault();
@@ -50,7 +86,8 @@ export default function App() {
     // formData.append("seletedFile", seletedFile);
     // console.log(formData);
     try {
-      const url = `https://v2.convertapi.com/convert/jpeg/to/webp?Secret=${process.env.REACT_APP_API_KEY}&StoreFile=true`;
+      setLoading(true);
+      const url = `https://v2.convertapi.com/convert/png/to/webp?Secret=${process.env.REACT_APP_API_KEY}&StoreFile=true`;
       const res = await axios.post(url, {
         headers: {
           "Content-Type": "application/json",
@@ -59,8 +96,8 @@ export default function App() {
           {
             Name: "File",
             FileValue: {
-              Name: "test.jpg",
-              Data: singleFile,
+              Name: seletedFileName,
+              Data: singleFileBase64,
             },
           },
           {
@@ -69,6 +106,8 @@ export default function App() {
           },
         ],
       });
+      setBtnLoading(false);
+      setLoading(false);
       console.log(res);
       console.log(res.data);
     } catch (error) {
@@ -77,30 +116,145 @@ export default function App() {
   };
   const handleFileSelect = (e) => {
     console.log(e.target.files[0].name);
-    setSelectedFile(e.target.files[0]);
+    setSelectedFileName(e.target.files[0]);
   };
   return (
     <>
       <ChakraProvider>
-        {/* <img src={`data:image/jpeg;base64,${Base64}`} alt="" /> */}
-        <form method="POST" onSubmit={convertJPEGtoWEBP}>
-          <FormControl>
-            {/* <input type="file" name="" id="" onChange={handleFileSelect} /> */}
-            <Flex direction="column">
-              <FormLabel>Select A File</FormLabel>
-              <FileBase
-                type="file"
-                multiple={false}
-                onDone={({ base64 }) =>
-                  setSingleFile({ ...singleFile, selectedFile: base64 })
-                }
-                onChange={handleFileSelect}
-              />
-              <Button type="submit">Submit</Button>
-            </Flex>
-          </FormControl>
-        </form>
+        <Container maxW={"1140px"}>
+          <Flex
+            bg={"gray.100"}
+            alignItems={"center"}
+            justifyContent="center"
+            h="100vh"
+            direction={"column"}
+            rowGap={10}
+          >
+            <Heading as={"h1"} textAlign="center">
+              Convert PNG to WEBP
+            </Heading>
+            <Box
+              border="2px dashed"
+              borderColor="gray.300"
+              p={25}
+              borderRadius={8}
+              w={"80%"}
+              cursor="pointer"
+              onClick={() =>
+                document.querySelector('input[type="file"]').click()
+              }
+            >
+              {singleFileBase64 ? (
+                <Flex justifyContent="center">
+                  <Image
+                    src={`data:image/jpeg;base64,${singleFileBase64}`}
+                    alt={seletedFileName}
+                    objectFit="contain"
+                    height={300}
+                  />
+                </Flex>
+              ) : (
+                <Flex
+                  textAlign={"center"}
+                  justifyContent={"center"}
+                  direction="column"
+                >
+                  <Image
+                    src={require("./assets/images/dummy-image.png")}
+                    w={300}
+                    m="auto"
+                  />
+                  <Heading
+                    mt={3}
+                    as={"h5"}
+                    color="gray.500"
+                    fontWeight="300"
+                    fontSize="md"
+                  >
+                    Click to select
+                  </Heading>
+                </Flex>
+              )}
+            </Box>
+            <form method="POST" onSubmit={convertJPEGtoWEBP}>
+              <FormControl>
+                {/* <input type="file" name="" id="" onChange={handleFileSelect} /> */}
+                <Flex direction="column">
+                  <FormLabel textAlign="center" textTransform="capitalize">
+                    {seletedFileName
+                      ? "File Name: " + seletedFileName.slice(0, -4)
+                      : "Select A File"}
+                  </FormLabel>
+                  <FileBase
+                    type="file"
+                    multiple={false}
+                    onDone={({ base64 }) =>
+                      setSingleFile({ ...singleFile, selectedFile: base64 })
+                    }
+                    onChange={handleFileSelect}
+                  />
+                  <Button
+                    isLoading={btnLoading}
+                    w={300}
+                    type="submit"
+                    bg={"green.400"}
+                    color="white"
+                    _hover={{ bg: "green.300" }}
+                  >
+                    Submit
+                  </Button>
+                </Flex>
+              </FormControl>
+            </form>
+          </Flex>
+          <DownloadTable isLoading={isLoading} title={seletedFileName} />
+        </Container>
       </ChakraProvider>
     </>
+  );
+}
+
+const DownloadTable = (props) => {
+  let isLoading = props.isLoading;
+  return isLoading ? (
+    <TableContainer>
+      <Table variant="simple">
+        <Tbody>
+          <Tr>
+            <Td>{props.seletedFileName}</Td>
+            <Td>
+              <Button>Download Now</Button>
+            </Td>
+          </Tr>
+        </Tbody>
+      </Table>
+    </TableContainer>
+  ) : (
+    ""
+  );
+};
+
+function ToastStatusExample() {
+  const toast = useToast();
+  const statuses = ["success", "error", "warning", "info"];
+
+  return (
+    <Wrap>
+      {statuses.map((status, i) => (
+        <WrapItem key={i}>
+          <Button
+            onClick={() =>
+              toast({
+                title: `${status} toast`,
+                status: status,
+                isClosable: true,
+              })
+            }
+          >
+            Show {status} toast
+          </Button>
+        </WrapItem>
+      ))}
+    </Wrap>
   );
 }
